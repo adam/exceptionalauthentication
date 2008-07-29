f8731de17cd0ab9b06e4210727ecffd642bac27b
Daniel Neighman <has.sox@gmail.com>
2008-07-30 00:18:17 +1000

    Adds strategies for login

--- origapp/lib/auth_session.rb
+++ origapp/lib/auth_session.rb
@@ -2,60 +2,112 @@
 # Mixin for the Merb::DataMapperSession which provides for authentication
 # 
 module Authentication
+  @@login_strategies = {}
+  @@active_strategies = []
+  
+  def self.reset_strategies_to_default!
+    login_strategies.clear
+    active_strategies.clear
+    
+    add_login_strategy(:salted_login) do 
+      user = User.first(:login => params[:login])
+      if user && user.crypted_password == User.encrypt(user.salt, params[:password])
+        user
+      else
+        nil
+      end
+    end
+  end # reset_strategies_to_default!
+  
+  def self.add_login_strategy(label, &block)
+    login_strategies[label] = block
+    active_strategies << label unless active_strategies.include?(label)
+  end #add_login_strategy
+  
+  def self.remove_login_strategy!(label)
+    login_strategies.delete(label)
+    active_strategies.reject!{|s| s == label}
+  end
+  
+  def self.login_strategies
+    @@login_strategies
+  end #login_strategies
+  
+  def self.active_strategies
+    @@active_strategies
+  end
+  
+  def self.active_strategies=(set)
+    raise "Should be an Array like object" unless set.respond_to?(:each)
+    raise "Active Strategies should not be empty" if set.empty?
+    raise "Strategy Not Registered" unless (set - @@active_strategies).empty?
+    @@active_strategies = set
+  end
+  
   module Session
     
-    ##
-    # @return [TrueClass, FalseClass]
-    # 
-    def authenticated?
-      !user.nil?
-    end
-
-    ##
-    # returns the active user for this session, or nil if there's no user claiming this session
-    # @returns [User, NilClass]
-    def user
-      @user ||= self[:user_id].blank? ? nil : User.get(self[:user_id])
+    def self.included(base)
+      base.send(:include, InstanceMethods)
+      base.send(:extend,  ClassMethods)
     end
     
-    ## 
-    # allows for manually setting the user
-    # @returns [User, NilClass]
-    def user=(user)
-      case user
-      when User
-        self[:user_id] = user.id
-        @user = user
-      else
-        abandon!
-      end
-      @user
-    end
-
-    ##
-    # retrieve the claimed identity and verify the claim
-    # 
-    # @param login [String] user's claimed identity
-    # @param password [String] password to verify against the claimed identity
-    # @return [User, NilClass] the verified user, or nil if verification failed
-    # @see User::encrypt
-    # 
-    def authenticate(login, password)
-      if login && password
-        possible_user = ::User.first(:login => login, :active => true) 
-        Merb.logger.info! "Claimed Identity: #{possible_user.inspect}"
-        self.user = possible_user if possible_user && possible_user.crypted_password == User.encrypt(possible_user.salt, password)
-      end
-    end
-
-    ##
-    # abandon the session, log out the user, and empty everything out
-    # 
-    def abandon!
-      @user = nil
-      delete
-      self
-    end
+    module ClassMethods    
+    end # ClassMethods
     
-  end
+    module InstanceMethods
+      ##
+      # @return [TrueClass, FalseClass]
+      # 
+      def authenticated?
+        !user.nil?
+      end
+
+      ##
+      # returns the active user for this session, or nil if there's no user claiming this session
+      # @returns [User, NilClass]
+      def user
+        @user ||= self[:user_id].blank? ? nil : User.get(self[:user_id])
+      end
+    
+      ## 
+      # allows for manually setting the user
+      # @returns [User, NilClass]
+      def user=(user)
+        case user
+        when User
+          self[:user_id] = user.id
+          @user = user
+        else
+          abandon!
+        end
+        @user
+      end
+
+      ##
+      # retrieve the claimed identity and verify the claim
+      # 
+      # @param login [String] user's claimed identity
+      # @param password [String] password to verify against the claimed identity
+      # @return [User, NilClass] the verified user, or nil if verification failed
+      # @see User::encrypt
+      # 
+      def authenticate(login, password)
+        if login && password
+          possible_user = ::User.first(:login => login, :active => true) 
+          Merb.logger.info! "Claimed Identity: #{possible_user.inspect}"
+          self.user = possible_user if possible_user && possible_user.crypted_password == User.encrypt(possible_user.salt, password)
+        end
+      end
+
+      ##
+      # abandon the session, log out the user, and empty everything out
+      # 
+      def abandon!
+        @user = nil
+        delete
+        self
+      end
+    end # InstanceMethods
+    
+  end # Session
 end
--- origapp/spec/lib/aut_session_spec.rb
+++ origapp/spec/lib/aut_session_spec.rb
@@ -10,6 +10,117 @@ describe Authentication::Session do
     @session = Merb::CookieSession.new( "", "sekrit")
   end
 
+  describe "module methods" do
+    before(:each) do
+      Authentication.reset_strategies_to_default!
+    end
+    
+    after(:all) do
+      Authentication.reset_strategies_to_default!
+    end
+    
+    describe "reset_strategies_to_default!" do
+      it "should reset to the :salted_login strategy" do
+        Authentication.reset_strategies_to_default!
+        Authentication.active_strategies.to_a.should == [:salted_login]
+        Authentication.login_strategies[:salted_login].should be_a_kind_of(Proc)
+      end
+      
+      it "should reset after adding another strategy" do
+        Authentication.add_login_strategy(:another_one){ "stuff" }
+        Authentication.active_strategies.should include(:another_one)
+        Authentication.login_strategies.keys.should include(:another_one)
+        Authentication.reset_strategies_to_default!
+        Authentication.active_strategies.should_not include(:another_one)
+        Authentication.login_strategies.keys.should_not include(:another_one)
+        Authentication.active_strategies.should include(:salted_login)
+      end
+    end
+    
+    describe "add_login_strategy" do
+      it "should add a finder strategy" do
+        Authentication.add_login_strategy(:a_different_one) do 
+          user = User.first(:login => params[:login])
+          if user && user.crypted_password == User.encrypt(user.salt, params[:password])
+            user
+          else
+            nil
+          end
+        end
+        Authentication.login_strategies[:a_different_one].should be_a_kind_of(Proc)
+        Authentication.active_strategies.map{|s| s}.should == [:salted_login, :a_different_one]
+      end
+    end
+    
+    describe "active_strategies=" do
+      before(:each) do
+        @set = [:salted_login]
+      end
+      
+      it "should accept an item if it responds to each" do
+        @set.stub!(:respond_to?).with(:each).and_return(true)
+        Authentication.active_strategies = @set
+        Authentication.active_strategies.should == @set
+      end
+      
+      it "should not accept an item if it does not respond to each" do
+        @set.stub!(:respond_to?).with(:each).and_return(false)
+        lambda do
+          Authentication.active_strategies = @set
+        end.should raise_error
+      end
+      
+      it "should not accept an item if it is empty?" do
+        lambda do
+          Authentication.active_strategies = []
+        end.should raise_error
+      end
+      
+      it "should not accept the set if it contains an item not registered" do
+        Authentication.login_strategies.keys.should_not include(:not_here)
+        lambda do
+          Authentication.active_strategies = [:salted_login, :not_here]
+        end.should raise_error
+      end
+      
+      it "should recieve the set if it contains only registered items" do
+        Authentication.login_strategies.keys.should include(:salted_login)
+        lambda do
+          Authentication.active_strategies = [:salted_login]
+        end.should_not raise_error
+      end      
+    end
+    
+    describe "remove_login_strategy!" do
+      before(:each) do
+        Authentication.reset_strategies_to_default!
+        Authentication.add_login_strategy(:test_login){"stuff"}
+      end
+      
+      after(:all) do
+        Authentication.reset_strategies_to_default!
+      end
+      
+      it "should remove a strategy" do
+        Authentication.login_strategies.keys.should include(:test_login)
+        Authentication.active_strategies.should include(:test_login)
+        Authentication.remove_login_strategy!(:test_login)
+        Authentication.login_strategies.keys.should_not include(:test_login)
+        Authentication.active_strategies.should_not include(:test_login)
+      end
+      
+      it "should be able to remove the defualt strategies" do
+        Authentication.login_strategies.keys.should include(:salted_login)
+        Authentication.active_strategies.should include(:salted_login)
+        Authentication.remove_login_strategy!(:salted_login)
+        Authentication.login_strategies.keys.should_not include(:salted_login)
+        Authentication.active_strategies.should_not include(:salted_login)
+      end
+    end
+    
+      
+  end
+
   describe "user" do
     it "should return nil if the :user_id is not set" do
       @session[:user_id].should be_nil
