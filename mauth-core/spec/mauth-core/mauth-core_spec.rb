de6ea681d7302c52e2fcc06c776dac0159dae678
Daniel Neighman <has.sox@gmail.com>
2008-08-19 11:51:44 +1000

    Adds rake goodness for running specs

--- mauth-core/spec/mauth-core/mauth-core_spec.rb
+++ mauth-core/spec/mauth-core/mauth-core_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/spec_helper'
+require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "mauth-core" do
   it "should do something" do
--- mauth-core/spec/spec_helper.rb
+++ mauth-core/spec/spec_helper.rb
@@ -5,7 +5,6 @@ require 'rubygems'
 require 'merb-core'
 require 'merb-core/dispatch/session/cookie'
 require 'spec' # Satisfies Autotest and anyone else not using the Rake tasks
-
 require 'mauth-core'
 
 Merb.start :environment => "test", :adapter => "runner"
